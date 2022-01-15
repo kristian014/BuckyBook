0494b56089e9ed0d06e7fbfbf180f899ef66259d
kristian014 <kristianbtc@gmail.com>
2022-01-15 12:57:27 +0000

    add order header n details

--- BuckyBook/Data/ApplicationDbContext.cs
+++ BuckyBook/Data/ApplicationDbContext.cs
@@ -26,8 +26,9 @@ namespace BuckyBook.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
-
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-        
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
---
+++ BuckyBook/Data/Migrations/20220114211411_addedOrderHeaderAndDetailToDb.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuckyBook.Data.Migrations
+{
+    public partial class addedOrderHeaderAndDetailToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShippingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderTotal = table.Column<double>(type: "float", nullable: false),
+                    OrderStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrackingNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Carrier = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaymentDueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SessionId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaymentIntentId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StreetAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderHeaders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderHeaders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "OrderHeaders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ac4c7e66-a4c3-2241-4a77-3c6ca1aa4456",
+                column: "ConcurrencyStamp",
+                value: "7094422e-638f-4970-a75b-28a7a1fcdbe4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ba9a9e88-a7c4-3361-5a68-3b6cb1aa5166",
+                column: "ConcurrencyStamp",
+                value: "1603b247-bc73-4667-9eb1-130f95e37380");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ca7a7e55-a6c3-2161-4a33-3c7cb3aa7866",
+                column: "ConcurrencyStamp",
+                value: "72d7c756-b7c8-4c8a-80f5-ab54e53d099b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ca9e7e82-f6f8-4353-9b45-3a9db1ba6176",
+                column: "ConcurrencyStamp",
+                value: "c41f5857-f4d4-42af-a33a-923cee10181f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6bc333d6-41cd-5539-ad37-a8c941bf66d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "f4a99bfe-42a7-43e2-a4a1-c334d2a11faf", "AQAAAAEAACcQAAAAEJ/EY8SvRshLoRs2HpLwhIW6+M6KXe7tqSwJWsMWp2jkhgQQ2eiTB9Z78l5EwKGS6w==", "9825b195-0f1c-4c4d-bb7b-2364f221bc8a" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6bd555a6-61fc-4339-bc37-a5c532bf99d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "eb0aacfb-7b47-4c89-840a-c93527c22082", "AQAAAAEAACcQAAAAEDhcDoR/SBjHcuc0R379nW33OA+TSSm+uvVnM3D1oB4PXory2lAbRIcmGwT3+lo8dg==", "259be62a-24b8-45ba-85c9-d5f4b69cf38d" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc22d7-83cc-4799-cf47-a2c222bc22d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "743d2c19-6e43-48ad-ba52-31eb76e7ba57", "AQAAAAEAACcQAAAAEI2PSH/0eWLp2kuR5tEk3U+zd+mV36p8MGH7vVI+76HpNIuJAHthK4QsxRy14vclZw==", "9713a947-145d-4e22-87bc-0d303c204903" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8ea993d6-11ab-4438-ac76-a9b951af77d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "ccc56da7-2eb9-41c3-82e1-e8b4abc1e681", "AQAAAAEAACcQAAAAEEnfmGktrJKnGqF6HGZuApJ4x1CoKTe19lzXGLjUjQLS274mNceGfkFIn1WoCCeFWg==", "28838525-7d1a-4f2a-9e3b-f60bc89f3768" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "OrderHeaders");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ac4c7e66-a4c3-2241-4a77-3c6ca1aa4456",
+                column: "ConcurrencyStamp",
+                value: "a0a1417a-692d-46c6-b5a1-293f242897e6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ba9a9e88-a7c4-3361-5a68-3b6cb1aa5166",
+                column: "ConcurrencyStamp",
+                value: "348717e4-8d21-41e8-979d-0c8eed515997");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ca7a7e55-a6c3-2161-4a33-3c7cb3aa7866",
+                column: "ConcurrencyStamp",
+                value: "be661177-8e07-45bd-8a0a-72729042e7d1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ca9e7e82-f6f8-4353-9b45-3a9db1ba6176",
+                column: "ConcurrencyStamp",
+                value: "e163dbc9-e76c-465f-9ded-95d56cbeec6c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6bc333d6-41cd-5539-ad37-a8c941bf66d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "8f20ec47-9ec3-4a21-bddc-bbb85f9c3616", "AQAAAAEAACcQAAAAEDjs0gVb/fVsnjaT6lmJi1CFr/R2BzqObITHmJT8Qmik/RRyDLKiN1U36TwzqCyZgg==", "a5aff700-9f6e-4029-b852-fb6521fe7e95" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6bd555a6-61fc-4339-bc37-a5c532bf99d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "0aba93f8-2606-4eae-ae03-541306d3ae45", "AQAAAAEAACcQAAAAEIFN3wXUG5CDNpTaZKxi8OY03aNzf2Q9JttufnidZ0NpyQbfPD3evN0gDJnTEt6E8g==", "822e9986-1814-45f9-bb48-e809818d906a" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6dc22d7-83cc-4799-cf47-a2c222bc22d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e72d3c16-a9ab-4535-b7b3-861c14e36301", "AQAAAAEAACcQAAAAEGD1EcYNz1VuDMutYDHF0z0fo3VRAjZnJfHSBwq0YzzGIxYR4JfFjj+pHAuf5BJdGg==", "b4e8ce39-f5c5-4ab1-bcd9-eb9264813d98" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8ea993d6-11ab-4438-ac76-a9b951af77d5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "03241565-1704-47cc-ae11-6e12dfc537f0", "AQAAAAEAACcQAAAAEHR434mUOy6Y8H3uN7cEoeng5WnGiHb7T6SdaFvJ+ZZyvXV/ZGIP78qzHmwSS6fq1g==", "75272cdc-f454-4130-a575-63b16c4b0b27" });
+        }
+    }
+}
